0a8c1122c407abbae505fb797ba526edfb13eb26
Alicia Sykes <alicia@omg.lol>
2024-03-05 23:34:10 +0000

    Update embed text

--- embed.js
+++ embed.js
@@ -250,7 +250,7 @@ function embedCodeToTarget(targetDiv, codeText, showBorder, showLineNumbers, sho
     const rawFileURLSplit = rawFileURL.split("/");
     meta.innerHTML = `<a target="_blank" href="${rawFileURL}" style="float:right">view raw</a>
 <a target="_blank" href="${fileURL}">${rawFileURLSplit[rawFileURLSplit.length - 1]}</a>
-delivered <span class="hide-in-phone">with ❤ </span>by <a target="_blank" href="${serviceProvider}">emgithub</a>`;
+delivered <span class="hide-in-phone">with ❤ </span>by <a target="_blank" href="${serviceProvider}">github-embed.as93.net</a>`;
     meta.classList.add("file-meta");
     if (!isDarkStyle) {
       meta.classList.add("file-meta-light");
